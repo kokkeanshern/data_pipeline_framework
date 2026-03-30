267d6c0ebb7989d1ba51726f6d9b4bfa88154001
kokkeanshern <kokkeanshern@gmail.com>
2026-03-30 20:33:45 +0800

    update directory structure

--- data_pipelines/data_pipelines/assets/bronze/assets.py
+++ data_pipelines/data_pipelines/assets/bronze/assets.py
@@ -14,13 +14,15 @@ def bronze_bitcoin(coingecko: CoinGeckoResource):
     The code below writes the raw response from the CoinGecko API to the local filesystem.
     ToDo: Implement minIO to simulate the experience of writing to S3.
     """
+
+    now = datetime.now(timezone.utc)
+
     # Create output directory
-    output_dir = "data/bronze/coingecko/bitcoin"
+    output_dir = f"data/bronze/coingecko/bitcoin/year={now.date().year}/month={now.date().month:02d}/day={now.date().day:02d}"
     os.makedirs(output_dir, exist_ok=True)
 
     # Write raw response as JSON
-    timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%d")
-    output_path = f"{output_dir}/{timestamp}.json"
+    output_path = f"{output_dir}/{now.strftime('%H%M%S')}.json"
 
     with open(output_path, "w") as f:
         json.dump(data.model_dump(), f)
@@ -37,13 +39,17 @@ def build_bronze_massive_asset(ticker: str):
     @asset(name=f"bronze_massive_{safe_name}")
     def _asset(massive: MassiveResource):
         data = massive.get_price_info(ticker=ticker)
-        output_dir = f"data/bronze/massive/{ticker}"
+
+        now = datetime.now(timezone.utc)
+
+        output_dir = f"data/bronze/massive/{safe_name}/year={now.date().year}/month={now.date().month:02d}/day={now.date().day:02d}"
         os.makedirs(output_dir, exist_ok=True)
-        timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%d")
-        output_path = f"{output_dir}/{timestamp}.json"
+        output_path = f"{output_dir}/{now.strftime('%H%M%S')}.json"
+
         serializable = [vars(d) for d in data]
         with open(output_path, "w") as f:
             json.dump(serializable, f)
+        
         return output_path
 
     return _asset
